3c8415b3f26ddfccafa9a72c7ed90fba7a2dd927
unknown <eefretsoul@gmail.com>
2015-05-05 02:03:45 -0400

    adding functionality to the scene

--- Assets/scripts/PlayerController.js
+++ Assets/scripts/PlayerController.js
@@ -1,25 +1,49 @@
 ﻿#pragma strict
 
 private var isDeactivated : boolean;
-private var movementCounts : int;
+private var sides : int;
 private var playerBody : Rigidbody2D;
+private var isInZone: boolean;
+private var playerAnim : Animator;
+var strength : float = 1.0;
 
 function Start () {
 	isDeactivated = false;
-	movementCounts = 0;
+	isInZone = false;
+	sides = 0;
 	playerBody = GetComponent(Rigidbody2D);
+	playerAnim = GetComponent(Animator);
 }
 
 function Update () {
-	if(Input.GetMouseButton(0)){
-		var posVec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		playerBody.AddForce(posVec, ForceMode2D.Impulse);		
+	if(Input.GetMouseButtonDown(0)){
+		if (isInZone) {
+			var posVec = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			playerBody.AddForce(posVec * strength, ForceMode2D.Impulse);
+			sides++;
+		} else {
+			sides--;
+		}
+		isDeactivated = sides == 3;
+		playerAnim.SetInteger("Sides",sides);
 	}
 }
 
 function OnCollisionEnter2D(Bam: Collision2D){
 	if(Bam.transform.tag == "spikes"){
-		Application.Quit();
-		UnityEditor.EditorApplication.isPlaying = false;
+		if(isDeactivated) {
+			Destroy(this.gameObject);
+		} else {
+			Application.Quit();
+			UnityEditor.EditorApplication.isPlaying = false;
+		}
 	}
+}
+
+function OnTriggerEnter2D(other: Collider2D) {
+	isInZone = true;
+}
+
+function OnTriggerExit2D(other) {
+	isInZone = false;
 }
